095be12c97b3c28a6a56a9579a5e635023d16cb3
ZhengjunHUO <firelouiszj@hotmail.com>
2022-08-11 10:48:06 +0200

    add filter

--- k8s/controller-runtime/demo/main.go
+++ k8s/controller-runtime/demo/main.go
@@ -8,22 +8,33 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 func main() {
+	// Prepare client
 	cl, err := client.New(config.GetConfigOrDie(), client.Options{})
 	if err != nil {
-		fmt.Println("Create client failed !")
+		fmt.Println("Create client failed: ", err)
 		os.Exit(1)
 	}
 	fmt.Println("Create client succeeded !")
 
-	poList := &corev1.PodList{}
-	if err = cl.List(context.Background(), poList, client.InNamespace("kube-system")); err != nil {
-		fmt.Println("List pods under ns kube-system failed !")
+	// Prepare label selector for control plane pods
+	slct, err := labels.Parse("component in (kube-apiserver, kube-scheduler, kube-controller-manager)")
+	if err != nil {
+		fmt.Println("Create selecter failed: ", err)
 		os.Exit(1)
 	}
 
+	// Get control plane pods in kube-system
+	poList := &corev1.PodList{}
+	if err = cl.List(context.Background(), poList, client.InNamespace("kube-system"), client.MatchingLabelsSelector{Selector: slct}); err != nil {
+		fmt.Println("List pods under ns kube-system failed: ", err)
+		os.Exit(1)
+	}
+
+	// Print out control plane pods' name
 	for _, po := range poList.Items {
 		fmt.Printf("Find pod[%s]\n", po.Name)
 	}
