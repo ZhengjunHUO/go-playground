e0ad5a250670fda961d8d1be900126f67578767a
ZhengjunHUO <firelouiszj@hotmail.com>
2025-04-30 10:22:37 +0200

    fix bug decompress tar.gz
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- compress/compress.go
+++ compress/compress.go
@@ -8,54 +8,64 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
 const (
-	pathToInput      = "input/toxic.csv"
-	pathToCompress   = "compressed/toxic.tar.gz"
-	pathToDecompress = "decompressed"
+	pathToInput = "input/toxic.csv"
+	//pathToCompress     = "compressed/grafana-7.3.7.tar.gz"
+	pathToCompress     = "compressed/ingress.tgz"
+	folderToDecompress = "decompressed"
 )
 
 func main() {
 	//compressTarGz()
-	decompressTarGz()
+	decompressTarGz(pathToCompress, folderToDecompress)
 }
 
-func decompressTarGz() {
+func decompressTarGz(tarPath, targetPath string) {
 	var compressed *os.File
 	var gzr *gzip.Reader
+	var tr *tar.Reader
 	var err error
 
-	if compressed, err = os.Open(pathToCompress); err != nil {
+	if compressed, err = os.Open(tarPath); err != nil {
 		log.Fatalln(err)
 	}
 	defer compressed.Close()
 
-	if gzr, err = gzip.NewReader(compressed); err != nil {
-		log.Fatalln(err)
+	if strings.HasSuffix(tarPath, ".gz") || strings.HasSuffix(tarPath, ".tgz") {
+		if gzr, err = gzip.NewReader(compressed); err != nil {
+			log.Fatalln(err)
+		}
+		defer gzr.Close()
+		tr = tar.NewReader(gzr)
+	} else {
+		tr = tar.NewReader(compressed)
 	}
-	defer gzr.Close()
-
-	tr := tar.NewReader(gzr)
 
 	for {
 		header, err := tr.Next()
-
 		if err == io.EOF {
 			break
 		}
-
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		name := fmt.Sprintf("%s/%s", pathToDecompress, header.Name)
+		name := fmt.Sprintf("%s/%s", targetPath, header.Name)
 		switch header.Typeflag {
 		case tar.TypeDir:
-			if err := os.Mkdir(name, 0755); err != nil {
+			if err := os.MkdirAll(name, os.FileMode(header.Mode)); err != nil {
 				log.Fatalln("Error occurred making dir: ", err)
 			}
 		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(name), 0755); err != nil {
+				log.Fatalln("Error occurred making dir: ", err)
+			}
+
+			//fmt.Printf("%s type mode: %v\n", name, header.Mode)
 			outFile, err := os.Create(name)
 			if err != nil {
 				log.Fatalln("Error occurred creating file: ", err)
@@ -64,6 +74,10 @@ func decompressTarGz() {
 
 			if _, err := io.Copy(outFile, tr); err != nil {
 				log.Fatalln("Error occurred dumping data: ", err)
+			}
+
+			if err := os.Chmod(name, os.FileMode(header.Mode)); err != nil {
+				log.Fatalln("Error occurred chmoding file ", name, err)
 			}
 		case tar.TypeSymlink:
 			os.Symlink(header.Linkname, name)
